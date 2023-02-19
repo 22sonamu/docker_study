7a71f6af01fa947041d357bc67957988a8d1894f
22sonamu <22sonamu@naver.com>
2023-02-19 15:06:51 +0900

    feat : update count , get count api 작성

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return 'Hellow World'
+@app.route('/update/<count>')
+def get_user_info(count):
+    return jsonify({"count": count}), 200
 
+@app.route("/get")
+def get():
+    return "get"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
 # docker build -t test .
 # docker run -d --rm test1
