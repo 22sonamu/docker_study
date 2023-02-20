237196d8b90e0392b4468eaafc99a31a79eef1d0
22sonamu <22sonamu@naver.com>
2023-02-21 02:42:22 +0900

    feat : react 코드 작성

---
+++ frontend/src/Counter.jsx
@@ -0,0 +1,24 @@
+
+//Counter.jsx
+import React, { useState } from "react";
+import axios from "axios";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+  const onDecrease = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </>
+  );
+};
+
+export default Counter;
