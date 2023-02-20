d0c9388abf4e6754a318115e6c73c817eb815221
22sonamu <22sonamu@naver.com>
2023-02-21 02:35:39 +0900

    fix : /get api return type int -> str로 변경

--- backend/backend.py
+++ backend/backend.py
@@ -14,12 +14,12 @@ def get_user_info(count):
 def get():
     conn = psycopg2.connect(host='db',port=5432, user='example', password='example', dbname='count')
     cur = conn.cursor()
-    sql = 'select * from count'
+    sql = 'select num_count from count'
     cur.execute(sql)
     row = cur.fetchone()
     conn.commit()
     conn.close()
-    return row[0]
+    return str(row[0])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
